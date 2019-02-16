309952139791f68a0c2c0fd93c8f9a2b51baaa18
Marcelo Dias <mardsystems@gmail.com>
2019-02-16 12:03:11 -0200

    Semi-refatorado (mediaQuery).

--- src/app/monstros/medidas/medidas.component.ts
+++ src/app/monstros/medidas/medidas.component.ts
@@ -1,5 +1,5 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatSort, MatTableDataSource } from '@angular/material';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable, of } from 'rxjs';
@@ -11,8 +11,16 @@ import { Balanca, Medida, OmronHBF214 } from './medidas.model';
 import { MedidasService } from './medidas.service';
 
 const columnDefinitions = [
-  { def: 'col1', showMobile: true },
-  { def: 'col2', showMobile: false },
+  { showMobile: true, def: 'foto' },
+  { showMobile: true, def: 'data' },
+  { showMobile: true, def: 'peso' },
+  { showMobile: true, def: 'gordura' },
+  { showMobile: false, def: 'gorduraVisceral' },
+  { showMobile: true, def: 'musculo' },
+  { showMobile: false, def: 'idadeCorporal' },
+  { showMobile: false, def: 'metabolismoBasal' },
+  { showMobile: true, def: 'indiceDeMassaCorporal' },
+  { showMobile: true, def: 'menu' },
 ];
 
 @Component({
@@ -20,62 +28,54 @@ const columnDefinitions = [
   templateUrl: './medidas.component.html',
   styleUrls: ['./medidas.component.scss']
 })
-export class MedidasComponent implements OnInit {
+export class MedidasComponent implements OnInit, OnDestroy {
   medidas$: Observable<Medida[]>;
+  monstro: Monstro;
   balanca: Balanca;
   loading = true;
   disabledWrite: boolean;
 
-  fullDisplayedColumns: string[] = ['foto', 'data', 'peso', 'gordura', 'gorduraVisceral', 'musculo',
-    'idadeCorporal', 'metabolismoBasal', 'indiceDeMassaCorporal', 'menu'];
-  minimalDisplayedColumns: string[] = ['foto', 'data', 'peso', 'gordura', 'musculo', 'indiceDeMassaCorporal', 'menu'];
-  displayedColumns: string[] = ['foto', 'data', 'peso', 'gordura', 'musculo', 'idadeCorporal', 'indiceDeMassaCorporal', 'menu'];
+  // fullDisplayedColumns: string[] = ['foto', 'data', 'peso', 'gordura', 'gorduraVisceral', 'musculo',
+  //   'idadeCorporal', 'metabolismoBasal', 'indiceDeMassaCorporal', 'menu'];
+  // minimalDisplayedColumns: string[] = ['foto', 'data', 'peso', 'gordura', 'musculo', 'indiceDeMassaCorporal', 'menu'];
+  // displayedColumns: string[] = ['foto', 'data', 'peso', 'gordura', 'musculo', 'idadeCorporal', 'indiceDeMassaCorporal', 'menu'];
+
   dataSource: any;
-
-  monstroId: string;
-  monstro: Monstro;
-
   @ViewChild(MatSort) sort: MatSort;
 
-  mobileQuery: MediaQueryList;
+  desktopQuery: MediaQueryList;
 
   constructor(
     private dialog: MatDialog,
     private route: ActivatedRoute,
-    private monstrosService: MonstrosService,
     private medidasService: MedidasService,
+    private monstrosService: MonstrosService,
     changeDetectorRef: ChangeDetectorRef,
     media: MediaMatcher
   ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-
-    // this.monstrosService.monstroLogado$.subscribe((monstroLogado) => {
-    //   this.monstroLogado = monstroLogado;
-    // });
-
     this.balanca = new OmronHBF214();
+
+    this.desktopQuery = media.matchMedia('(min-width: 600px)');
+
+    // this.desktopQueryListener = () => changeDetectorRef.detectChanges();
+
+    this.desktopQuery.addListener(this.desktopQueryListener);
   }
 
-  private _mobileQueryListener(ev: MediaQueryListEvent) {
-    // if (ev.matches) {
-    //   this.fullDisplayedColumns.forEach(element => {
-    //     this.displayedColumns.push(element);
-    //   });
-    // } else {
-    //   this.minimalDisplayedColumns.forEach(element => {
-    //     this.displayedColumns.push(element);
-    //   });
-    // }
+  private desktopQueryListener(e: MediaQueryListEvent) {
+    // this.changeDetectorRef.detectChanges();
+  }
+
+  isDesktop(): boolean {
+    return this.desktopQuery.matches;
   }
 
   ngOnInit() {
     const monstro$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
-        this.monstroId = params.get('monstroId');
+        const monstroId = params.get('monstroId');
 
-        return this.monstrosService.obtemMonstroObservavel(this.monstroId);
+        return this.monstrosService.obtemMonstroObservavel(monstroId);
       }),
       catchError((error, monstro) => {
         console.log(error);
@@ -121,46 +121,23 @@ export class MedidasComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    this.desktopQuery.removeListener(this.desktopQueryListener);
+  }
+
   getDisplayedColumns(): string[] {
-    const isMobile = this.isMobile();
+    const isDesktop = this.isDesktop();
 
-    if (isMobile) {
-      return this.minimalDisplayedColumns;
-    } else {
-      return this.fullDisplayedColumns;
-    }
+    // if (isMobile) {
+    //   return this.minimalDisplayedColumns;
+    // } else {
+    //   return this.fullDisplayedColumns;
+    // }
 
-    // return this.columnDefinitions
-    //   .filter(cd => !isMobile || cd.showMobile)
-    //   .map(cd => cd.def);
+    return columnDefinitions
+      .filter(cd => isDesktop || cd.showMobile)
+      .map(cd => cd.def);
   }
-
-  isMobile(): boolean {
-    return this.mobileQuery.matches;
-  }
-
-  // novaMedida(): void {
-  //   const medida: Medida = {
-
-  //   };
-
-  //   medida.monstroId = 'monstros/vQeCUnaAWmzr2YxP5wB1';
-  //   medida.data = new Date();
-  //   medida.peso = 0;
-  //   medida.gordura = 0;
-  //   medida.gorduraVisceral = 0;
-  //   medida.musculo = 0;
-  //   medida.idadeCorporal = 0;
-  //   medida.metabolismoBasal = 0;
-  //   medida.indiceDeMassaCorporal = 0;
-
-  //   this.medidasService.cadastraMedida(medida);
-  // }
-
-  // atualizaMedida(medida: Medida): void {
-  //   // task.done = !task.done;
-  //   this.medidasService.atualizaMedida(medida);
-  // }
 
   onAdd(): void {
     const model = MedidaViewModel.toAddViewModel(this.monstro);
