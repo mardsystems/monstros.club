023e6b1b42ad13c6e79a539a722907f6921a0b31
Marcelo Dias <mardsystems@gmail.com>
2019-03-11 23:58:06 -0300

    Treinos (v2).

--- src/app/monstros/treinos/treinos.domain-model.ts
+++ src/app/monstros/treinos/treinos.domain-model.ts
@@ -1,5 +1,36 @@
-export class Treino {
+export class Tempo {
+  valor: number;
+}
+
+export class Modalidade {
   id: string;
-  monstroId: string;
-  data: number;
+  nome: string;
+}
+
+export abstract class Treino {
+  id: string;
+  data: Date;
+  modalidade: Modalidade;
+  blocos: string;
+  duracao: Tempo;
+}
+
+export class TreinoBase extends Treino {
+  ativo: boolean;
+
+  ativa(): void {
+    this.ativo = true;
+  }
+
+  desativa(): void {
+    this.ativo = false;
+  }
+}
+
+export class ExecucaoDeTreino extends Treino {
+  base: TreinoBase;
+
+  diferencaExecutada(): number {
+    return this.duracao.valor - this.base.duracao.valor;
+  }
 }
