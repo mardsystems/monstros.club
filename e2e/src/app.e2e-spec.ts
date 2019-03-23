0461b38e564f3da8e7ce1f39e632cbe996ce7e2d
Marcelo Dias <mardsystems@gmail.com>
2019-03-23 10:52:19 -0300

    Atualização dos testes.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to monstros-club!');
+    expect(page.getTitleText()).toEqual('Monstros Club');
   });
 });
--- src/app/admin/monstros/listagem/listagem.component.spec.ts
+++ src/app/admin/monstros/listagem/listagem.component.spec.ts
@@ -1,16 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListagemComponent } from './listagem.component';
+import { AdminComponent } from '../../admin.component';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { MonstrosComponent } from '../monstros.component';
+import { MedidasComponent } from '../medidas/medidas.component';
+import { CadastroComponent } from '../../cadastro/cadastro.component';
+import { AdminMaterialModule } from '../../admin-material.module';
+import { AdminRoutingModule } from '../../admin-routing.module';
+import { AppCommonModule } from 'src/app/app-common.module';
+import { AngularFirestore, FirestoreSettingsToken, AngularFirestoreModule } from '@angular/fire/firestore';
+import { LOCALE_ID } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { RouterModule, ActivatedRoute, Router } from '@angular/router';
+
+// let activatedRoute: ActivatedRouteStub;
 
 describe('ListagemComponent', () => {
   let component: ListagemComponent;
   let fixture: ComponentFixture<ListagemComponent>;
 
   beforeEach(async(() => {
+    const routerSpy = createRouterSpy();
+
     TestBed.configureTestingModule({
-      declarations: [ ListagemComponent ]
+      imports: [
+        AdminMaterialModule,
+        AdminRoutingModule,
+        AppCommonModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule.enablePersistence(),
+        RouterModule
+        // AppRoutingModule
+      ],
+      providers: [
+        // { provide: ActivatedRoute, useValue: activatedRoute },
+        // { provide: HeroService, useClass: TestHeroService },
+        { provide: Router, useValue: routerSpy },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -23,3 +53,7 @@ describe('ListagemComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+function createRouterSpy() {
+  return jasmine.createSpyObj('Router', ['navigate']);
+}
